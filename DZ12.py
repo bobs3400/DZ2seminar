1a51ccd5ddbbc3f9b053d190e69033355fd499a7
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-05-25 22:05:20 +1000

    12

--- DZ12.py
+++ DZ12.py
@@ -1,2 +1,10 @@
 sum = int(input("Введите сумма двух чисел: "))
 compos = int(input("Введите произведение двух чисел: "))
+mean = False
+for x in range(1, 1000):
+    for y in range(1, 1000):
+        if sum == x + y and compos == x * y:
+            mean = True
+            print(x, y)
+if mean == False:
+    print("Нет таких чисел")   
